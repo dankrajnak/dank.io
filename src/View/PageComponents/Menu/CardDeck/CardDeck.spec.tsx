2eaa12b9269df12132939ad6b9c4a81dd20ff722
Daniel Krajnak <dan.krajnak@gmail.com>
2019-10-03 16:02:45 -0700

    Delete unuseful Card, Card Deck tests

--- src/View/PageComponents/Menu/CardDeck/CardDeck.spec.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as renderer from "react-test-renderer";
-import CardDeck from "./CardDeck";
-
-it("renders with no cards", () => {
-  const tree = renderer.create(<CardDeck width={300} cards={[]} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
