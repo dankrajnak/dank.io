8ea270e28711c5ea2cb2f05909f91fe06aeb16e6
Daniel Krajnak <dan.krajnak@gmail.com>
2020-01-25 13:31:46 -0500

    Fix menu interface prop types

--- src/Domain/Menu/Menu.tsx
+++ src/Domain/Menu/Menu.tsx
@@ -1,8 +1,21 @@
 import * as React from "react";
 import Card from "../Card/Card";
+import { Location } from "history";
 import { RouteComponentProps } from "@reach/router";
 
+type PropsPassedThroughLocationState = {
+  prevPath: string | null;
+};
+/**
+ * This is the best I can do, but probably flawed.  At times, pages will store the previousPath
+ * on location state.  This is an attempt to somewhat override RouteComponentProps from the router
+ * to include this property.
+ */
+export type MenuRouteProps = RouteComponentProps<{
+  location: Location<PropsPassedThroughLocationState> & Window["location"];
+}>;
+
 export type Menu = React.FunctionComponent<{
-  routeProps: RouteComponentProps;
+  routeProps: MenuRouteProps;
   cards: Card[];
 }>;
