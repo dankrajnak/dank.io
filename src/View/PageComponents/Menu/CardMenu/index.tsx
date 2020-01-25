726121fb5dfd8a2a1aaad5e34013ab73bd07dec2
Daniel Krajnak <dan.krajnak@gmail.com>
2020-01-25 14:15:36 -0500

    Revert "Test husky"
    This reverts commit 07893ac8f0ba1a951e1675b127166274614d1587.

--- src/View/PageComponents/Menu/CardMenu/index.tsx
+++ src/View/PageComponents/Menu/CardMenu/index.tsx
@@ -65,8 +65,6 @@ const CardMenu: Menu = ({ routeProps, cards }) => {
   if (flash) {
     return flash;
   }
-  // Introduce intentional type error to check husky.
-  const thing: string = 3;
   return (
     <>
       <AboutContainer>
